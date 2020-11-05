6a87faec0754f23f99443279da9bcfa66706b980
Claudia Quintana Wong <claudiaqw94@gmail.com>
2020-11-05 13:09:33 +0100

    exercise 2 manipulation dplyr finished

--- practica_programacion_R_2.R
+++ practica_programacion_R_2.R
@@ -18,7 +18,8 @@ data <- unite(data, course_id_title, course_id, course_title, sep = "_", remove 
 
 
 #2. Sobreescribe el valor de las columnas published_timestamp por una columna de tipo fecha.
-data["published_timestamp"] <- as.Date(data$published_timestamp)
+# data["published_timestamp"] <- as.Date(data$published_timestamp) -> information about hour,day,minute, timezone is lost
+data["published_timestamp"] <- as.POSIXct(data$published_timestamp, format = "%y-%m-%dT%h:%m:%s", tz = Sys.timezone())
 
 
 #3. Crea un long dataset ... (from wide to long)
@@ -54,5 +55,71 @@ original_dataset_reshape2 = dcast(long_dataset_reshape2,
 ######################################################
 ### Ejercicio 2 - Manipulación (dplyr, data.table) ###
 ######################################################
+
+library(dplyr)
+#1. precio medio de los cursos dependiendo de su temática y ordena el resultado según el precio medio
+data  %>% 
+  group_by(subject) %>% 
+  summarise(price_mean = mean(price)) %>% 
+  arrange(desc(price_mean))
+
+#2. Calcula la duración máxima y mínima dependiendo de si un curso es gratuito o de pago.
+data %>% 
+  group_by(is_paid) %>% 
+  summarise(min = min(content_duration), max = max(content_duration))
+  
+#3. Calcula el número de cursos publicado cada año. 
+data %>% 
+  mutate(year = format(data$published_timestamp, format="%Y")) %>% 
+  group_by(year) %>% 
+  summarise(courses_per_year = n())
+  
+
+#4. Cuál es la temática que tiene el mayor número de clases medias
+data %>% 
+  group_by(subject) %>% 
+  summarize(class_mean = mean(num_lectures)) %>% 
+  arrange(desc(class_mean)) %>% 
+  slice(1)
+
+
+#5. Restringiéndonos a los cursos lanzados en 2016, ¿qué temática cuenta con más horas de clase?
+data %>% 
+  mutate(year = format(data$published_timestamp, format="%Y")) %>% 
+  filter(year == "2016") %>% 
+  group_by(subject) %>% 
+  summarise(hours_of_classes = sum(content_duration)) %>%
+  arrange(desc(hours_of_classes)) %>% 
+  slice(1)
+
+
+#6. Para todos los cursos posteriores a 2015, calcula las horas del curso más largo, del más corto y el número de estudiantes medio.
+data %>% 
+  mutate(year = as.numeric(format(data$published_timestamp, format="%Y"))) %>% 
+  filter(year > 2015) %>% 
+  group_by(year) %>% 
+  summarise(longest_course = max(content_duration), shortest_course = min(content_duration), students_mean = mean(num_subscribers))
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
